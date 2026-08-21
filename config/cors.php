df2b5bfe85efd6267a4a2708f7eb22a2da09d6d9
Jayvee.Dev <hidlaojayvee18@gmail.com>
2026-08-21 16:13:54 +0800

    Fix: default cors origin

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        env('APP_ALLOWED_ORIGINS',"")  
+        env('APP_ALLOWED_ORIGINS',"https://expenses-be-cgd9.onrender.com")  
     ],
 
     'allowed_origins_patterns' => [],
